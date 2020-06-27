7ccea300224797fe09dcbac8b9a1e68950b79414
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2020-06-27 16:23:38 -0300

    adicionando javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+var checkbox = document.querySelector("#checkbox")
+var basic = document.querySelector("#price--basic")
+var pro = document.querySelector("#price--pro")
+var master = document.querySelector("#price--master")
+
+checkbox.addEventListener('change', function(){
+    if(this.checked){
+        master.innerHTML = "<b>$</b>139,99"
+        pro.innerHTML = "<b>$</b>124,99"
+        basic.innerHTML = "<b>$</b>119,99"
+    }
+    else{
+        basic.innerHTML = "<b>$</b>39,99"
+        pro.innerHTML = "<b>$</b>24,99"
+        master.innerHTML = "<b>$</b>39,99"
+    }
+})
